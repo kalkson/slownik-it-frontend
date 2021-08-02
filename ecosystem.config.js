45fbe862cdddbe258d25b17a57295234487bd30b
Damian Kalka <kalka762@gmail.com>
2021-08-02 22:00:35 +0200

    fix space

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  apps: [{
-    name: "itglossary",
-    script: "node_modules/next/dist/bin/next",
-    args: "start",
-    exec_mode: "cluster",
-    env: {
-      PM2_SERVE_PORT: 8900
-    }
-  }]
+  apps: [
+    {
+      name: 'itglossary',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      exec_mode: 'cluster',
+      env: {
+        PM2_SERVE_PORT: 8900,
+      },
+    },
+  ],
 }
