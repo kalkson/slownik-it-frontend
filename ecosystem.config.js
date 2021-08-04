7ce4140c50f8032999dfa7e5af5ca7c63781cfdb
Damian Kalka <56680144+kalkson@users.noreply.github.com>
2021-08-04 23:08:15 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'itglossary',
-      cwd: '.',
+      cwd: '/var/www/itglossary.pl/_work/slownik-it-frontend/slownik-it-frontend',
       script: 'npm',
       args: 'start',
       env: {
